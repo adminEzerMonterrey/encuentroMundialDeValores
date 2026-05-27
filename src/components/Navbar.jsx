ece0c8b8e94ba04a1caa75b5e130463d4320f8f8
Ethan Rivera <ethan.rivera@udem.edu>
2026-05-27 17:26:04 -0600

    style: increase 2026 size and rename Ponentes to Panelistas in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
           <span></span><span></span><span></span>
         </button>
         <ul className={`nav__menu ${isOpen ? 'open' : ''}`} id="navMenu" role="list">
-          <li><a href="#speakers" className="nav__link" onClick={closeMenu}>Ponentes</a></li>
+          <li><a href="#speakers" className="nav__link" onClick={closeMenu}>Panelistas</a></li>
           <li><a href="#agenda" className="nav__link" onClick={closeMenu}>Agenda</a></li>
           <li><a href="#sponsors" className="nav__link" onClick={closeMenu}>Colaboradores</a></li>
           <li><a href="#ubicacion" className="nav__link" onClick={closeMenu}>Ubicación</a></li>
